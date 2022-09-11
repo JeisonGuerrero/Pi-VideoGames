b75dab8dbd3dabc33fb6dd57c4d496bf8c148129
JeisonGuerrero <jeison.g.s@hotmail.com>
2022-09-10 20:00:32 -0500

    HASTA POST

--- api/src/Controller/Controller.js
+++ api/src/Controller/Controller.js
@@ -19,15 +19,75 @@ const infoApi = async () => {
                 description
             }
         })
-        const result = await Promise.all(promesa)
+        const result = await Promise.all(promesa.flat())
         return result
     } catch (error) {
         console.log(error, 'Error en el infoApi')
     }
 };
 
+const dbInfo = async () => {
+    try {
+        const db = await Videogame.findAll({
+            includes: [{
+                model: Genre,
+                attributes: ['name']
+            }],
+            through: [{
+                attributes: []
+            }]
+        })
+        return db;
+    } catch (error) {
+        console.log(error, 'Error en la dbInfo')
+    }
+};
+
+const allInfo = async () => {
+    const apiInfo = await infoApi();
+    const infoDb = await dbInfo();
+    const infoAll = infoDb.concat(apiInfo);
+    
+    return infoAll;
+};
+
+const getByGame = async (game) => {
+    try {
+        const response = await axios(`https://api.rawg.io/api/games?search=${game}&key=43ab0952436047b1a77b4a696628fd5a&page_size=15`);
+        const promesa = response.data.results.map(async ele => {
+            const infoId = await axios(`https://api.rawg.io/api/games/${ele.id}?key=43ab0952436047b1a77b4a696628fd5a`)
+            const description = infoId.data.description.replace(/<[^>]+>/ig, '');
+            return {
+                id: ele.id,
+                name: ele.name,
+                released: ele.released,
+                rating: ele.rating,
+                plataforms: ele.platforms.map(s => s.platform.name),
+                image: ele.background_image,
+                genre: ele.genres.map(s => s.name),
+                description
+            }
+        })
+        const result = await Promise.all(promesa.flat())
+        return result
+    } catch (error) {
+        console.log(error, 'Error en Get By Name');
+    }
+};
+
+const getById = async (id) => {
+    try {
+        const resId = await allInfo();
+        const result = resId.find((ele)=> ele.id == id);
+        return result;
+    } catch (error) {
+        console.log(error, 'Error en GetById');
+    }
+};
 
 
 module.exports = {
-    infoApi,
+    allInfo,
+    getByGame,
+    getById,
 }
--- api/src/routes/GamesRoute.js
+++ api/src/routes/GamesRoute.js
@@ -1,5 +1,6 @@
 const { Router } = require ('express');
-const { infoApi } = require ('../Controller/Controller.js')
+const { allInfo, getByGame, getById } = require ('../Controller/Controller.js')
+const { Videogame, Genre } = require('../db');
 
 // import all controllers
 // import SessionController from './app/controllers/SessionController';
@@ -9,18 +10,62 @@ const routes = new Router();
 // Add routes
 routes.get('/', async (req, res) => {
     try {
-        const games = await infoApi();
-        res.status(200).send(games)
-        
+        const { name } = req.query;
+        if (name) {
+            const query = await getByGame(name);
+            query.length?
+            res.status(200).send(query):
+            res.status(404).send('Game not found');
+        }else{
+            const videogames = await allInfo();
+            res.status(200).send(videogames)
+        };
     } catch (error) {
         console.log(error, 'Error en el Get videogames')
     }
 });
+
 routes.get('/:id', async (req, res) => {
-    res.status(200).send('Soy la ruta del get games by id')
+        const { id } = req.params;
+        const query = await getById(id);
+       if (query) {
+        res.status(200).send(query);
+       } else {
+        res.status(404).send(' Id Not Found');
+       }
 });
+
 routes.post('/', async (req, res) => {
-    res.status(200).send('Soy la ruta del post games')
+    try {
+        const {
+            id,
+            name,
+            released,
+            rating,
+            plataforms,
+            image,
+            genre,
+            description
+        } = req.body;
+        const newGame = await Videogame.create({
+            id, 
+            name, 
+            released,
+            rating,
+            plataforms, 
+            image,
+            description
+        });
+        const newGenre = await Genre.findAll({
+            where: { name: genre }
+        })
+    
+        await newGame.addGenre(newGenre);
+    
+        res.status(200).send('Your Videogame has been created successfully');
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 
